c951fe4417e8df1bc48358991cad48ccc12432a0
Fergus <fergus.jingtai@gmail.com>
2018-07-20 16:31:56 +0800

    7-8 actionCreators 与 constants 的拆分

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+import { actionCreators } from './store';
 import {
   HeaderWrapper,
   Logo,
@@ -57,16 +58,10 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   handleInputFocus() {
-    const action = {
-      type: 'search_focus'
-    };
-    dispatch(action);
+    dispatch(actionCreators.searchFocus());
   },
   handleInputBlur() {
-    const action = {
-      type: 'search_blur'
-    };
-    dispatch(action);
+    dispatch(actionCreators.searchBlur());
   }
 });
 
