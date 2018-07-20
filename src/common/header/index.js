26aa263f4b87bb357b8b4258ead08a6395cc532c
Fergus <fergus.jingtai@gmail.com>
2018-07-20 11:47:44 +0800

    7-3 使用 styled-components 完成 Header 组件布局（2）

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import {
   HeaderWrapper,
-  Logo
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button
 } from './style';
 
 class Header extends Component {
@@ -9,6 +14,17 @@ class Header extends Component {
     return (
       <HeaderWrapper>
         <Logo />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">登陆</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <NavSearch />
+        </Nav>
+        <Addition>
+          <Button className="reg">写文章</Button>
+          <Button className="writting">注册</Button>
+        </Addition>
       </HeaderWrapper>
     );
   }
