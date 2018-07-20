3b6c386fbb281b681a5f2cc92831d9038ecd5ee3
Fergus <fergus.jingtai@gmail.com>
2018-07-20 16:51:09 +0800

    7-10 使用 redux-immutable 统一数据格式

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -53,7 +53,7 @@ const Header = props => (
 );
 
 const mapStateToProps = state => ({
-  focused: state.header.get('focused')
+  focused: state.getIn(['header', 'focused'])
 });
 
 const mapDispatchToProps = dispatch => ({
