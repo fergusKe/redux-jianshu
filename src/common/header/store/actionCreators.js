d1eb5925881275b1b9cac92c9160795a30fceba4
fergusKe <fergus.jingtai@gmail.com>
2018-07-21 16:34:50 +0800

    7-14 热门搜索换页功能实现

--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -5,7 +5,8 @@ import * as constants from './constants';
 
 const changeList = data => ({
   type: constants.CHANGE_LIST,
-  data: fromJS(data)
+  data: fromJS(data),
+  totalPage: Math.ceil(data.length / 10)
 });
 
 export const searchFocus = () => ({
@@ -26,3 +27,16 @@ export const getList = () => (dispatch) => {
       console.log('error');
     });
 };
+
+export const mouseEnter = () => ({
+  type: constants.MOUSE_ENTER
+});
+
+export const mouseLeave = () => ({
+  type: constants.MOUSE_LEAVE
+});
+
+export const pageChange = page => ({
+  type: constants.PAGE_CHANGE,
+  page
+});
