df46abf5a17eeb0443a1fb304a82c48fc76d2976
fergusKe <fergus.jingtai@gmail.com>
2018-07-22 23:35:18 +0800

    8-3 首页专题区域布局及reducer的设计

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { TopicWrapper, TopicItem } from '../style';
 
 class Topic extends Component {
   render() {
+    const { list } = this.props;
     return (
-      <div>
-        Topic
-      </div>
+      <TopicWrapper>
+        {
+          list.map(item => (
+            <TopicItem key={item.id}>
+              <img
+                className="topic-pic"
+                src={item.get('imgUrl')}
+                alt=""
+              />
+              {item.get('title')}
+            </TopicItem>
+          ))
+        }
+      </TopicWrapper>
     );
   }
 }
 
-export default Topic;
+const mapStateToProps = state => ({
+  list: state.getIn(['home', 'topicList'])
+});
+
+export default connect(mapStateToProps, null)(Topic);
