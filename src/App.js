b3f86df8b5bc8650dcadd9c09c39a3b95f1d2e5c
Fergus <fergus.jingtai@gmail.com>
2018-07-24 16:47:02 +0800

    9-5 登陆页面布局

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header';
 import Home from './pages/home';
 import Detail from './pages/detail';
+import Login from './pages/login';
 import store from './store';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
             <Header />
             <Route exact path="/" component={Home} />
             <Route path="/detail/:id" exact component={Detail} />
+            <Route path="/login" exact component={Login} />
           </div>
         </BrowserRouter>
       </Provider>
