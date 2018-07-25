076e30d6d29499b2a105344700ddfe6b7fc1318b
Fergus <fergus.jingtai@gmail.com>
2018-07-25 15:59:27 +0800

    9-8 异步组件及withRouter路由方法的使用

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { DetailWrapper, Header, Content } from './style';
 import { actionCreators } from './store';
 
@@ -33,4 +34,4 @@ const mapDispatchToProps = dispatch => ({
   }
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Detail);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Detail));
