d47715748e209a70a38e49b1bb9d0cccb9ba8d48
Fergus <fergus.jingtai@gmail.com>
2018-07-25 15:44:15 +0800

    9-7 登陆鉴权及代码优化

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './common/header';
 import Home from './pages/home';
 import Detail from './pages/detail';
 import Login from './pages/login';
+import Write from './pages/write';
 import store from './store';
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/detail/:id" exact component={Detail} />
             <Route path="/login" exact component={Login} />
+            <Route path="/write" exact component={Write} />
           </div>
         </BrowserRouter>
       </Provider>
